46b39764af79cafbe6dc80eff4556e25448ec6cc
nano-dynamite <100312084+nano-dynamite@users.noreply.github.com>
2023-06-30 21:54:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl.js')
+const app = express();
+
+const dbUrl = 'mongodb://localhost/urlShortener';
+
+mongoose.connect(dbUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(() => {
+		console.log('Connected to the database.');
+	})
+	.catch((error) => {
+		console.log('Error occurred while connecting to the database:', error);
+	});
+
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/', async (req, res) => {
+	const shortUrls = await ShortUrl.find()
+	res.render('index' , {shortUrls : shortUrls});
+});
+
+
+app.post('/shortUrls', async (req, res) => {
+	await ShortUrl.create({ full: req.body.fullUrl })
+
+	res.redirect('/')
+});
+
+app.get('/:shortUrl', async (req, res) => {
+	const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+	if (shortUrl == null) return res.sendStatus(404)
+  
+	shortUrl.clicks++
+	shortUrl.save()
+  
+	res.redirect(shortUrl.full)
+  })
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log('Server is running.');
+});
